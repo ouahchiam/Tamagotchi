b6f2d226b9136e9083604686874738326b98a25e
ouahchiam <147640505+ouahchiam@users.noreply.github.com>
2023-11-23 20:44:45 +0000

    Update Tamagotchi.swift

--- Tamagotchi/Tamagotchi.swift
+++ Tamagotchi/Tamagotchi.swift
@@ -14,17 +14,23 @@ class Tamagotchi {
         "age": 0,
         "weight": 1,
         "disciplineMeter": 100, // descending
-        "attention": 0,
+        "attention": 100,
         "health": 100,
     ]
     let feedAmount: Double = 10
     let disciplineAmount: Double = 20
     private let debounces: [String: Double] = [ // debounce in seconds
-        "discipline" : 20,
+        "disciplineMeter" : 20,
         "defecate" : 20,
     ]
-    private var lastTriggered: [String: Double] = [
-        "discipline": Date()
+    private var lastTriggered: [String: Date] = [
+        "disciplineMeter": Date(),
+        "defecate": Date(),
+    ]
+    private let heartbeatDecrement: [String: Double] = [
+        "hunger": 1,
+        "disciplineMeter": -1,
+        "attention": -0.5
     ]
     
     init(weight: Double) {
@@ -32,23 +38,68 @@ class Tamagotchi {
     }
     
     func returnDiff(date1: Date, date2: Date) -> Double {
-        return CFDateGetTimeIntervalSinceDate(date1, date2)
+        return date1.timeIntervalSince(date2)
     }
     
     func feed() {
         if properties["hunger"]! > feedAmount {
             properties["hunger"]! -= feedAmount
+            properties["bowels"]! += 20
         }
         else {
             properties["hunger"] = 0
+                properties["bowels"]! += 2
         }
     }
     
     func defecate() {
+        if returnDiff(date1: lastTriggered["defecate"]!, date2: Date()) > debounces["defecate"]! {
+            print("Debounce active")
+            return
+        }
         properties["bowels"] = 2
     }
     
     func discipline() {
-        properties["discipline"]! += disciplineAmount
+        if returnDiff(date1: lastTriggered["disciplineMeter"]!, date2: Date()) > debounces["disciplineMeter"]! {
+            print("Debounce active")
+            return
+        }
+        if properties["disciplineMeter"]! < (100 -  disciplineAmount) {
+            properties["disciplineMeter"]! += disciplineAmount
+        }
+        else {
+            properties["disciplineMeter"]! = 100
+        }
+    }
+    
+    func outputProperties() {
+        for (property, amount) in properties {
+            print("\(property.uppercased()) is currently at \(amount).")
+        }
+    }
+    
+    func heartbeat() {
+        for (property, amount) in heartbeatDecrement {
+
+                if amount > 0 { // amount is positive (i.e. works its way up to 100)
+                    if properties[property]! < (100 - amount) {
+                        properties[property]! += amount
+                    }
+                    else {
+                        properties[property]! = 100
+                    }
+                }
+                else {
+                    if properties[property]! > (amount) {
+                        properties[property]! += amount
+                    }
+                    else {
+                        properties[property]! = 0
+                    }
+                }
+            
+            
+        }
     }
 }
