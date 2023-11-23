2c0048b1fd59022bdc8cd7f4b2deab76b9295fc6
ouahchiam <147640505+ouahchiam@users.noreply.github.com>
2023-11-23 20:44:58 +0000

    Update main.swift

--- Tamagotchi/main.swift
+++ Tamagotchi/main.swift
@@ -7,3 +7,35 @@
 
 import Foundation
 
+
+let heartbeatInSeconds = 1.0
+let pet = Tamagotchi(weight: 2)
+
+print("Enter 1 to feed, 2 to defecate, 3 to discipline, 4 to output properties.")
+
+func logic() async {
+    while (true) {
+        do {
+            try await Task.sleep(nanoseconds: UInt64(heartbeatInSeconds * Double(NSEC_PER_SEC)))
+            pet.heartbeat()
+            let str = readLine()
+            if str == "1" {
+                pet.feed()
+            }
+            else if str == "2" {
+                pet.defecate()
+            }
+            else if str == "3" {
+                pet.discipline()
+            }
+            else if str == "4" {
+                pet.outputProperties()
+            }
+        }
+        catch {
+            print("oops")
+        }
+    }
+}
+
+ await logic()
